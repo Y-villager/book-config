e57e6ff819c01f4d223509d5b1e2fdd2ed98c51d
yeyu <178798826@qq.com>
2024-03-26 14:49:17 +0800

    Initial commit

---
+++ src/main/java/com/yxz/bookconfig/BookConfigApplication.java
@@ -0,0 +1,13 @@
+package com.yxz.bookconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookConfigApplication.class, args);
+	}
+
+}
